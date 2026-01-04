6eafce96c22a7e9e85a9600ccde6d5e56219397b
Tawhid Talal <tawhid.talal@gmail.com>
2026-01-03 19:10:39 -0500

    fixing media image url in serializer

--- backend-drf/products/serializers.py
+++ backend-drf/products/serializers.py
@@ -3,10 +3,11 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+    
     class Meta:
         model = Product
         fields = '__all__'
         
     def get_image(self, obj):
-        print("Serializing image for product:", obj.name, "Image field:", obj.image)
         return obj.image.url if obj.image else None
